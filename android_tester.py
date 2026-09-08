8d2d3bc18c0c244b8249fba0d2d996a9e33c8c9e
Salahuddin Khan <salahuddin.khan.1994@gmail.com>
2026-09-08 00:03:58 -0400

    Initial android controller

---
+++ android_tester.py
@@ -0,0 +1,39 @@
+from appium import webdriver
+from appium.options.android import UiAutomator2Options
+import time
+
+DEVICE_UDID = "127.0.0.1:6555"
+HINGE_PACKAGE = "co.hinge.app"
+
+options = UiAutomator2Options()
+options.platform_name = "Android"
+options.automation_name = "UiAutomator2"
+options.device_name = DEVICE_UDID
+options.udid = DEVICE_UDID
+
+# Don't reset app data/login state.
+options.no_reset = True
+
+driver = webdriver.Remote(
+    "http://127.0.0.1:4723",
+    options=options,
+)
+
+try:
+    print("Connected to Android.")
+
+    print("Launching Hinge...")
+    driver.activate_app(HINGE_PACKAGE)
+
+    time.sleep(5)
+
+    print("Current package:", driver.current_package)
+    print("Current activity:", driver.current_activity)
+
+    print("\nUI hierarchy:")
+    print(driver.page_source)
+
+    input("\nHinge should now be open. Press Enter to quit...")
+
+finally:
+    driver.quit()
